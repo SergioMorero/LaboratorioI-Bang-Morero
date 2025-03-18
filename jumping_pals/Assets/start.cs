14ec3502fb83f59a6d4be57fc50f6775b929e956
bang_santiago <sbang@mail.austral.edu.ar>
2025-03-17 21:37:17 -0300

    Added sprites and placeholders Added functional main title screen

---
+++ jumping_pals/Assets/start.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class start : MonoBehaviour
+{
+    public void StartGame() {
+        SceneManager.LoadSceneAsync(0);
+    }
+}
