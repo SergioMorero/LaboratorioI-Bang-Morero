f3f32e9cde3b76dd3667796c660908817f7db3c9
SergioMorero <smorero@mail.austral.edu.ar>
2025-04-27 19:13:05 -0300

    Added 'acvhievement renderer manager' script that shows achievements

---
+++ jumping_pals/Assets/Scripts/achvRendererManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UIElements;
+
+public class Achievements : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class Achievement
+    {
+        public int id;
+        public string name;
+    }
+
+    [System.Serializable]
+    public class Wrapper
+    {
+        public Achievement[] array;
+    }
+
+    private int userID;
+    private TextMeshProUGUI achievementsText;
+    private string serverUrl = "http://localhost:5000";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        userID = PlayerPrefs.GetInt("ID");
+        achievementsText = GetComponent<TextMeshProUGUI>();
+        achievementsText.text = "";
+        StartCoroutine(ShowAchievements());
+    }
+
+    IEnumerator ShowAchievements()
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(serverUrl + "/achievements/" + userID))
+        {
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                string json = request.downloadHandler.text;
+                // Lo 'wrappeo' para que Unity pueda leerlo bien
+                string newJson = "{\"array\": " + json + "}";
+                Wrapper wrapper = JsonUtility.FromJson<Wrapper>(newJson);
+                Achievement[] achievements = wrapper.array;
+
+                foreach (Achievement achv in achievements)
+                {
+                    achievementsText.text += $"{achv.name}\n";
+                }
+
+            }
+            else
+            {
+                Debug.LogError("Error: " + request.error);
+            }
+
+        }
+    }
+    
+}
