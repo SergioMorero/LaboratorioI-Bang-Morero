be0da55479d0aa8538acdfc994beb8e41559c021
bang_santiago <sbang@mail.austral.edu.ar>
2025-05-18 18:26:44 -0300

    Added alternative sprites Added more prefabs Moved prefabs into different folders

--- jumping_pals/Assets/Scripts/PlayerSpriteManager.cs
+++ jumping_pals/Assets/Scripts/PlayerSpriteManager.cs
@@ -7,21 +7,42 @@ public class PlayerSpriteManager : MonoBehaviour
 {
 
     public int CharSelected;
+    public int alt;
     public CharacterDB CharDB;
     public Character character;
     public SpriteLibrary SpriteSource;
     public SpriteResolver resolver;
+    
+    public string player;
+    private KeyCode left;
+    private KeyCode right;
+    private KeyCode up;
 
-    void Start()
-    {
+    void Start() {
         CharSelected = PlayerPrefs.GetInt("CharId");
-        LoadSprite();
+        character = CharDB.getChar(CharSelected);
+        alt = PlayerPrefs.GetInt("alt");
+        player = gameObject.name;
+        switch (player) {
+            case "player1":
+                SpriteSource.spriteLibraryAsset = character.sprites;
+                break;
+            case "player2":
+                SpriteSource.spriteLibraryAsset = character.altSprites;
+                break;
+            default:
+                LoadSprite();
+                break;
+        }
     }
 
-    void LoadSprite()
-    {
+    void LoadSprite() {
         character = CharDB.getChar(CharSelected);
-        SpriteSource.spriteLibraryAsset = character.sprites;
+        if (alt == 0) {
+            SpriteSource.spriteLibraryAsset = character.sprites;
+        } else {
+            SpriteSource.spriteLibraryAsset = character.altSprites;
+        }
     }
 
 }
